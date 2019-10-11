5138d3d5d8f44ceda8d7cfff210584b8f2e00ae5
Parz1val1 <35855311+Parz1val1@users.noreply.github.com>
2019-10-11 00:02:31 -0400

    created more UI and scenes as well as scene management in level select

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    private string level1 = "Level1", level2 = "Level2", level3 = "Level3", level4 = "Level4";
+    public GameObject startUI, levelSelectUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelSelectUI.SetActive(false);
+        startUI.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadLevel1()
+    {
+        SceneManager.LoadScene(level1);
+    }
+
+    public void loadLevel2()
+    {
+        SceneManager.LoadScene(level2);
+    }
+
+    public void loadLevel3()
+    {
+        SceneManager.LoadScene(level3);
+    }
+
+    public void loadLevel4()
+    {
+        SceneManager.LoadScene(level4);
+    }
+
+    public void play()
+    {
+        startUI.SetActive(false);
+        levelSelectUI.SetActive(true);
+    }
+}
