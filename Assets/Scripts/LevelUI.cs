43c647d96b5a2bf15b82736f5a25e89b9704c6bd
Parz1val1 <35855311+Parz1val1@users.noreply.github.com>
2019-10-13 00:46:24 -0400

    second work session, made level one and added animations for player

---
+++ Assets/Scripts/LevelUI.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class LevelUI : MonoBehaviour
+{
+    private string playText = "►", pauseText = "II", menu = "StartScene";
+    public GameObject pauseScreen;
+    public TextMeshProUGUI pauseButton;
+    public Image[] hearts = new Image[3];
+    public TextMeshProUGUI arrows;
+    public int arrowNum, playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseScreen.SetActive(false);
+        arrowNum = Player.arrows;
+        playerHealth = Player.health;
+        arrows.text = ("x" + arrowNum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateArrows();
+        updateHealth();
+    }
+
+    public void pause()
+    {
+        if (Player.paused)
+        {
+            Player.paused = false;
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+            pauseButton.text = pauseText;
+        }
+        else
+        {
+            Player.paused = true;
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+            pauseButton.text = playText;
+        }
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(menu);
+    }
+
+    public void updateArrows()
+    {
+        if (Player.arrows != arrowNum)
+        {
+            arrowNum = Player.arrows;
+            arrows.text = ("x" + arrowNum);
+        }
+    }
+
+    public void updateHealth()
+    {
+        if(playerHealth != Player.health)
+        {
+            playerHealth = Player.health;
+            hearts[playerHealth].sprite = Resources.Load<Sprite>("Tiny RPG Forest/Artwork/sprites/misc/hearts/hearts-2");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static int health, arrows;
+    public static bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+        arrows = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
